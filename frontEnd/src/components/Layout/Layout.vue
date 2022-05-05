c31da707a21b4650d5612280ced60fb1bd4f0e7b
Insiro <leeon@insiro.me>
2022-05-05 12:07:47 +0000

    add: update layout

--- frontEnd/src/components/Layout/Layout.vue
+++ frontEnd/src/components/Layout/Layout.vue
@@ -2,6 +2,15 @@
   <div>
     <div id="wrapper">
       <SideBar />
+      <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+          <NavBar />
+          <div class="container-fluid">
+            <slot />
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
     <!-- Logout Modal-->
   </div>
@@ -10,7 +19,7 @@
 import { Vue, Options } from "vue-class-component";
 import SideBar from "./sidebar";
 import Footer from "./Footer.vue";
-
-@Options({ components: { SideBar, Footer } })
+import NavBar from "./navbar/NavBar.vue";
+@Options({ components: { SideBar, Footer, NavBar } })
 export default class Layout extends Vue {}
 </script>
