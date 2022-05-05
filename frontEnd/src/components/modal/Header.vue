dccd833862e54a6a3f1ed9e777a15bfc92bd5950
Insiro <leeon@insiro.me>
2022-05-05 12:33:58 +0000

    add: Modal base

---
+++ frontEnd/src/components/modal/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="modal-header">
+    <h5 class="modal-title"><slot /></h5>
+    <button type="button" @click="closeModal()">
+      <span aria-hidden="true">×</span>
+    </button>
+  </div>
+</template>
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Header extends Vue {
+  closeModal() {
+    this.$emit("on-close-click");
+  }
+}
+</script>
---
+++ frontEnd/src/components/modal/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout @on-close-req="closeReq()">
+    <template #header>{{ title }}</template>
+    <template #body><slot /></template>
+    <template #footer>
+      <div class="btn btn-primary" @click="okClicked()">Ok</div>
+    </template>
+  </Layout>
+</template>
+<script lang="ts">
+import { Vue, prop } from "vue-class-component";
+class Props {
+  title = prop<string>({ default: "" });
+}
+export default class LogOut extends Vue.with(Props) {
+  okClicked() {
+    this.$emit("on-ok-click");
+  }
+  closeReq() {
+    this.$emit("on-close-req");
+  }
+}
+</script>
