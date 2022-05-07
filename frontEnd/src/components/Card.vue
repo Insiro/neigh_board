2276fc3fab536f821f7ff44ca42509ec795ec1de
Insiro <leeon@insiro.me>
2022-05-07 08:15:08 +0000

    add: card element

---
+++ frontEnd/src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card shadow mb-4">
+    <div v-if="header !== null" class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary">{{ header }}</h6>
+    </div>
+    <div class="card-body">
+      <slot />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, prop } from "vue-class-component";
+class Prop {
+  header = prop<string | null>({ default: null });
+}
+export default class Card extends Vue.with(Prop) {}
+</script>
