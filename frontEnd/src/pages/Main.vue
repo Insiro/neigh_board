4941a6367f23eba80afe1a17a2798a570f327bff
Insiro <leeon@insiro.me>
2022-05-07 08:16:12 +0000

    add: table on main page

--- frontEnd/src/pages/Main.vue
+++ frontEnd/src/pages/Main.vue
@@ -1,7 +1,25 @@
 <template>
-  <div />
+  <Card header="head">
+    <Table>
+      <template #header>
+        <tr>
+          <th scope="row">#</th>
+          <th scope="row">title</th>
+        </tr>
+      </template>
+      <template #content>
+        <tr>
+          <th scope="column">1</th>
+          <td>title-1</td>
+        </tr>
+      </template>
+    </Table>
+  </Card>
 </template>
 <script lang="ts">
-import { Vue } from "vue-class-component";
+import { Vue, Options } from "vue-class-component";
+import Card from "../components/Card.vue";
+import Table from "../components/Table.vue";
+@Options({ components: { Table, Card } })
 export default class Main extends Vue {}
 </script>
