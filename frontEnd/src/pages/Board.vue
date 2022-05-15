81f82fbfef31825d270dfe1949d988172d74adc9
Insiro <leeon@insiro.me>
2022-05-15 21:15:07 +0900

    add: bord and post schefold

---
+++ frontEnd/src/pages/Board.vue
@@ -0,0 +1,45 @@
+<template>
+  <Card header="head">
+    <Table>
+      <template #header>
+        <tr>
+          <th scope="row">#</th>
+          <th scope="row">title</th>
+        </tr>
+      </template>
+      <template #content>
+        <tr
+          v-for="(item, index) in pages"
+          :key="item.id"
+          @click="moveSub(item.id)"
+        >
+          <th scope="column">{{ index }}</th>
+          <td>{{ item.title }}</td>
+        </tr>
+      </template>
+    </Table>
+  </Card>
+</template>
+<script lang="ts">
+import { Vue, Options } from "vue-class-component";
+import Card from "../components/Card.vue";
+import Table from "../components/Table.vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+interface page {
+  id: string;
+  title: string;
+}
+@Options({ components: { Table, Card } })
+export default class Board extends Vue {
+  pages: Array<page> = [];
+  router = useRouter();
+  beforeMount() {
+    this.pages.push({ id: "id", title: "title" });
+    this.pages.push({ id: "id2", title: "title2" });
+  }
+  moveSub(id: string) {
+    this.router.push("/post/" + id);
+  }
+}
+</script>
---
+++ frontEnd/src/pages/Post.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ id }}
+  </div>
+</template>
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { useRoute } from "vue-router";
+export default class Component extends Vue {
+  id: string = "ids";
+  beforeMount() {
+    //
+    this.id = useRoute().params.id.toString();
+  }
+}
+</script>
