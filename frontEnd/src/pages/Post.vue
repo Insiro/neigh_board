4cfebd3512f60e5f36b79878429f1cad7360acdc
Insiro <leeon@insiro.me>
2022-05-15 21:46:01 +0900

    add: markdown viewer for post

--- frontEnd/src/pages/Post.vue
+++ frontEnd/src/pages/Post.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    {{ id }}
+    <div>{{ title }}</div>
+    <!--eslint-disable-next-line vue/no-v-html-->
+    <div id="marked" class="marked mt-5" v-html="postText"></div>
   </div>
 </template>
 <script lang="ts">
@@ -8,9 +10,14 @@ import { Vue } from "vue-class-component";
 import { useRoute } from "vue-router";
 export default class Component extends Vue {
   id: string = "ids";
+  title = "";
   beforeMount() {
     //
     this.id = useRoute().params.id.toString();
+    this.title = this.id;
+  }
+  get postText(): string {
+    return "";
   }
 }
 </script>
