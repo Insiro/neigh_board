6920bb56c24e4e957e7541b3b6f627d33b9c07a8
Gihyun Kim <83570546+kimgihean@users.noreply.github.com>
2022-05-15 19:11:28 +0900

    Create Comment.java

---
+++ Neighboard/SE/src/main/java/com/SE/spring/model/Comment.java
@@ -0,0 +1,31 @@
+package com.seproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="comment")
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne
+	@JoinColumn(name="user_id")// user_id 외래키
+	private User user;
+	@ManyToOne
+	@JoinColumn(name="board_pk")// 게시글 pk 외래키
+	private WebBoard webBoard;
+	
+	@Column(length = 200)
+	private String comment; //댓글 내용
+	
+	private LocalDateTime register_date;// 댓글 단 날짜
+	
+}
