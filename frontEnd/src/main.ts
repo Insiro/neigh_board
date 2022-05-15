037842ae20d153bd28d219ca1d2eb160b3111972
Insiro <leeon@insiro.me>
2022-05-15 21:16:51 +0900

    add: states

--- frontEnd/src/main.ts
+++ frontEnd/src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "normalize.css";
+import store from "./store";
 import router from "./router";
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount("#app");
