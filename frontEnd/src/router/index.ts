931e41596552b4a7c312730228d1f06ddb6e0df3
Insiro <leeon@insiro.me>
2022-05-15 21:15:47 +0900

    update: routers

--- frontEnd/src/router/index.ts
+++ frontEnd/src/router/index.ts
@@ -2,29 +2,37 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Main from "../pages/Main.vue";
 import Auth from "@/pages/auth/Auth.vue";
 import Register from "@/pages/auth/Register.vue";
-// import Admin from "../views/Admin.vue";
+import Board from "@/pages/Board.vue";
+import NewPost from "@/pages/NewPost.vue";
+import Post from "@/pages/Post.vue";
 
 const authRoutes: Array<RouteRecordRaw> = [
+  { path: "/", component: Main },
   {
     path: "/auth",
     component: Auth,
-    meta: {
-      noLayout: true,
-    },
+    meta: { noLayout: true },
   },
   {
     path: "/regist",
     component: Register,
-    meta: {
-      noLayout: true,
-    },
+    meta: { noLayout: true },
+  },
+  {
+    path: "/post",
+    component: Board,
+  },
+  {
+    path: "/newPost",
+    component: NewPost,
+  },
+  {
+    path: "/post/:id",
+    component: Post,
   },
 ];
 
-const routes: Array<RouteRecordRaw> = [
-  { path: "/", component: Main, meta: { noLayout: false } },
-  ...authRoutes,
-];
+const routes: Array<RouteRecordRaw> = [...authRoutes];
 
 const router = createRouter({
   history: createWebHistory(),
