560196ede7f8a8b8aad9ec61b59e597929c46278
Insiro <leeon@insiro.me>
2022-05-21 16:31:07 +0900

    fix: change to as_view()

--- back/board/urls.py
+++ back/board/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("user", views.UserController.as_view),
-    path("auth", views.AuthController.as_view),
+    path("user", views.UserController.as_view()),
+    path("auth", views.AuthController.as_view()),
 
 ]
