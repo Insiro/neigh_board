eaf4b54da48ec6148f8d9afe1392703f4fa29525
Insiro <leeon@insiro.me>
2022-05-21 17:51:53 +0900

    fix: models cascade errors add: board app

--- back/back/settings.py
+++ back/back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7k#_$&pe010_8#rtthq(fk3=)^_$7$)pp!ymzwq@^+)b^%h29&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "board.apps.BoardConfig"
 ]
 
 MIDDLEWARE = [
--- back/board/models.py
+++ back/board/models.py
@@ -1,36 +1,39 @@
+from django.db import models
 from datetime import datetime
 from uuid import uuid4
-from django.db import models
-
 
 # Create your models here.
 
 
 class Region(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, primary_key=True)
 
 
 class User(models.Model):
     user_id = models.CharField(max_length=100, primary_key=True)
+    user_name = models.CharField(max_length=40)
     certification = models.CharField(max_length=100)
-    register = models.DateField(default=datetime.now())
+    register = models.DateField(default=datetime.now)
     phone = models.CharField(max_length=20)
-    region = models.ForeignKey(Region, null=False)
+    region = models.ForeignKey(Region, null=True, on_delete=models.SET_NULL)
     salt = models.TextField()
 
 
 class Post(models.Model):
-    board_id = models.UUIDField(primary_key=True, uuid=uuid4, editable=False)
-    author = models.ForeignKey(User, null=False)
+    board_id = models.UUIDField(
+        primary_key=True, default=uuid4, editable=False)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=30)
     content = models.TextField()
     likes = models.IntegerField(default=0)
-    region = models.ForeignKey(Region, null=False)
+    region = models.ForeignKey(Region, null=True, on_delete=models.SET_NULL)
+    date = models.DateField(default=datetime.now)
 
 
 class Comment(models.Model):
-    pk = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    comment_id = models.UUIDField(
+        primary_key=True, default=uuid4, editable=False)
     post = models.ForeignKey(Post, null=False, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, null=True)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     comment = models.TextField()
-    date = models.DateField(default=datetime.now())
+    date = models.DateField(default=datetime.now)
