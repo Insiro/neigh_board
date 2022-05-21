575083f8588c007437d1a9c18571a51b688634d2
Insiro <leeon@insiro.me>
2022-05-21 16:29:54 +0900

    add: salt on models

--- back/board/models.py
+++ back/board/models.py
@@ -16,6 +16,7 @@ class User(models.Model):
     register = models.DateField(default=datetime.now())
     phone = models.CharField(max_length=20)
     region = models.ForeignKey(Region, null=False)
+    salt = models.TextField()
 
 
 class Post(models.Model):
