6128ea59c40509ff8f3b1de765cbb560ba689798
Insiro <leeon@insiro.me>
2022-05-31 03:22:13 +0900

    add: Post and Comment Controller

--- back/board/post_api.py
+++ back/board/post_api.py
@@ -1,62 +1,123 @@
-
 from datetime import datetime
+from http.client import UNAUTHORIZED, UNPROCESSABLE_ENTITY
 from django.views import View
-from django.http import Http404, HttpResponseBadRequest, JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.hashers import check_password
 from django.forms.models import model_to_dict
-from board.models import Post, Comment
+from board.httpResponse import HttpError
+from board.models import Post, Comment, User, Region
+import json
 
 
 class PostController(View):
     def post(self, request, *args, **kwargs):
-        post = self.request.POST
-        author = self.request.session["id"]
-        Post(
+        if not "id" in request.session:
+            return HttpError("UNAUTHORIZED", UNAUTHORIZED)
+        body = json.loads(request.body)
+        user_id = self.request.session["id"]
+        author = User.objects.get(user_id=user_id)
+        region = Region.objects.get(name=request.session['region'])
+        Post.objects.create(
             author=author,
-            region=request.session['region'],
-            title=post.get("title"),
-            content=post.get("content"),
+            region=region,
+            title=body.get("title"),
+            content=body.get("content"),
             likes=0,
             date=datetime.today()
         ).save()
         return JsonResponse({"result": "success"}, json_dumps_params={"ensure_ascii": False})
 
     def get(self, request, *args, **kwargs):
-        if "region" in self.request.session:
-            all_board = model_to_dict(Post.objects.filter(
-                region=self.request.session["region"]))
-        else:
-            all_board = model_to_dict(Post.objects.all())  # 게시판 전체 다 보여주기
-        return JsonResponse({"board": all_board}, json_dumps_params={"ensure_ascii": False})
+        all_post = []
+        if "all" in self.request.GET:
+            all_post = Post.objects.all()  # 게시판 전체 다 보여주기
+        elif "region" in self.request.session:
+            all_post = Post.objects.filter(
+                region=self.request.session["region"])
+
+        posts = []
+        for item in all_post:
+            post = model_to_dict(item)
+            post["id"] = item.board_id
+            posts.append(post)
+        return JsonResponse({"posts": posts}, json_dumps_params={"ensure_ascii": False})
 
 
 class PostItemController(View):
+    def dispatch(self, request, *args, **kwargs):
+        self.id = kwargs["id"]
+        try:
+            self.post = Post.objects.get(board_id=kwargs["id"])
+            return super().dispatch(request, args, kwargs)
+        except:
+            return HttpResponse("NOTFOUND", status=404)
+
     def delete(self, request, *args, **kwargs):
-        pass
+        body = json.loads(request.body)
+        try:
+            if not "pwd" in body or not "id" in request.session:
+                raise HttpError("UNAUTHORIZED", UNAUTHORIZED)
+            if not check_password(body.get("pwd"), self.post.author.certification):
+                raise HttpError("UNAUTHORIZED", UNAUTHORIZED)
+            self.post.delete()
+            return HttpResponse("success")
+        except HttpError as error:
+            error.send()
+        except ObjectDoesNotExist:
+            return HttpResponse("NOTFOUND", status=404)
 
     def get(self, request, *args, **kwargs):
-        pass
+        return JsonResponse({"post": model_to_dict(self.post)}, json_dumps_params={"ensure_ascii": False})
+
+
+class PostCommentItems(View):
+    def dispatch(self, request, *args, **kwargs):
+        self.post_id = kwargs["post_id"]
+        try:
+            self.posts = Post.objects.get(board_id=self.post_id)
+            return super().dispatch(request, args, kwargs)
+        except ObjectDoesNotExist:
+            return HttpResponse("NOTFOUND", status=404)
+
+    def get(self, request, *args, **kwargs):
+        comment_list = Comment.objects.filter(post=self.posts)
+        comments = []
+        for item in comment_list:
+            post = model_to_dict(item)
+            post["id"] = item.comment_id
+            comments.append(post)
+        return JsonResponse({"comments": comments}, json_dumps_params={"ensure_ascii": False})
+
+    def post(self, request, *args, **kwargs):
+        body = json.loads(request.body)
+        try:
+            if not "id" in request.session:
+                raise HttpError("UNAUTHORIZED", UNAUTHORIZED)
+            if not "comment" in body:
+                raise HttpError("UNPROCESSABLE_ENTITY", UNPROCESSABLE_ENTITY)
+            if not "comment" in body:
+                raise HttpError("UNPROCESSABLE_ENTITY", UNPROCESSABLE_ENTITY)
+            user = User.objects.get(user_id=request.session["id"])
+            Comment.objects.create(
+                post=self.posts,
+                author=user,
+                comment=body["comment"],
+                date=datetime.now()
+            )
+            return HttpResponse("success")
+        except HttpError as e:
+            e.send()
 
 
 class CommentController(View):
-    def post(self, request, args, **kwargs):
-        # 댓글 객체를 만들어주는 역할
-        post = self.request.POST
-        if not("id" in kwargs and "comment" in post and "author" in post):
-            return HttpResponseBadRequest()
+    def dispatch(self, request, *args, **kwargs):
+        self.id = kwargs.get("id")
+        try:
+            self.coms = Comment.objects.get(comment_id=self.id)
+            return super().dispatch(request, args, kwargs)
+        except ObjectDoesNotExist:
+            return HttpResponse("NOTFOUND", status=404)
 
-        comment = Comment(post=kwargs["id"], comment=post.get("comment"),
-                          author=post.get("author"), date=datetime.now())
-        comment.save()
-
-        return JsonResponse({"new_comment": comment}, json_dumps_params={"ensure_ascii": False})
-
-    def get(self, request, args, **kwargs):  # 댓글을 보여주는 코드, 해당 게시물의 post_id만 갖는 댓글만 필터링하여 저장하고
-        #comment.post == board.board_id
-        comm_id = kwargs.get('id')
-        if comm_id is None:
-            comments = Comment.objects.filter(comment_id=comm_id)
-
-            return JsonResponse({"board": model_to_dict(comments)}, json_dumps_params={"ensure_ascii": False})
-        else:
-            return Http404
-
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({"comment": model_to_dict(self.coms)}, json_dumps_params={"ensure_ascii": False})
--- back/board/urls.py
+++ back/board/urls.py
@@ -5,21 +5,20 @@ from board import post_api
 from . import api
 
 # 클래스형 뷰는 클래스로 진입하기 위한 as_view() 클래스 메소드로 진입메소드라 부름
-# 클래스형 뷰에는 내부적으로 dispatch() 메소드가 있기 때문에 
-#dispatch()메소드가 get,post등의 어떤 HTTP 메소드로 요청을 중계해줌
+# 클래스형 뷰에는 내부적으로 dispatch() 메소드가 있기 때문에
+# dispatch()메소드가 get,post등의 어떤 HTTP 메소드로 요청을 중계해줌
 
 # 제네릭 뷰를 상속받아서 클래스형 뷰를 만들기 위하여 urlspatterns에 .as_view( )를 호출
 urlpatterns = [
     path("user/<str:user_id>", api.get_user_info),
     path("user", api.UserController.as_view()),
     path("auth", api.AuthController.as_view()),
-
-    path("comment/<str:id>", post_api.CommentController.as_view()),
     path("register", api.Register),
-
-
     path("post", post_api.PostController.as_view()),
     path("post/<str:id>", post_api.PostItemController.as_view()),
-    path("comment", post_api.CommentController.as_view())
+    path("post/<str:post_id>/comment/<str:id>",
+         post_api.CommentController.as_view()),
+    path("post/<str:post_id>/comment", post_api.PostCommentItems.as_view()),
+    path("comment/<str:id>", post_api.CommentController.as_view())
 
 ]
