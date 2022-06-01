aa4bd749a4834ca741619011b872c0a56c484989
Insiro <leeon@insiro.me>
2022-06-01 06:19:51 +0000

    add: StyledCard

---
+++ frontEnd/src/components/StyledButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="btn btn-primary btn-user btn-block">
+    <slot />
+  </button>
+</template>
+<script lang="ts">
+import { Vue } from "vue-class-component";
+export default class StyledButton extends Vue {}
+</script>
--- frontEnd/src/components/card/CardB.vue
+++ frontEnd/src/components/card/CardB.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-xl-3 col-md-6 mb-4">
+  <div class="col-xl-6 col-md-8 mb-6 cardContainer">
     <div class="card border-left-primary shadow h-100 py-2">
       <div class="card-body">
         <slot />
@@ -11,3 +11,10 @@
 import { Vue } from "vue-class-component";
 export default class CardBB extends Vue {}
 </script>
+
+<style scoped>
+.cardContainer {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+</style>
